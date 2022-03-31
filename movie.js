53e0dc07a8a871c70bbbfd31cef2e01e7411b0d9
hodanyusuff <98726595+hodanyusuff@users.noreply.github.com>
2022-03-31 15:12:54 -0600

    Add files via upload

--- movie.js
+++ movie.js
@@ -4,6 +4,8 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let User = require('../Users');
+
+
 chai.should();
 
 chai.use(chaiHttp);
@@ -48,3 +50,26 @@ describe('Register, Login and Call Movie with Basic Auth and JWT Auth', () => {
         })
     });
 });
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+
+//Movie schema
+var MovieSchema = new Schema({
+    title: { type: String, required: true, index: { unique: true }},
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+    genre: {type: string, required: true, index: {unique: true}},
+    actors: {type: string, required: true, index: {unique: true}},
+
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
+
+
