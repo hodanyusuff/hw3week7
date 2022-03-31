5e92f211e61061b7c3a68f0cf5d7236b6797c454
hodanyusuff <98726595+hodanyusuff@users.noreply.github.com>
2022-03-31 15:13:18 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var authController = require('./auth');
 var authjwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
-var User = require('./users');
+var User = require('./Users');
+
 
 require('dotenv').config({ path: './.env' });
 
@@ -82,6 +83,47 @@ router.post('/signin', function (req,res){
         })
     })
 });
+
+router.route('/movie')// this is all movies
+    .post(authController.isAuthenticated, function(req, res) {
+            console.log(req.body);
+            res = res.status(200);
+            if(req.get('content-Type')) {
+                res = res.type(req.get('content-Type'));
+            }
+            var o = getJSONObjectForMovieRequirement(req);
+            res.json(o);
+        }
+    )
+    .get(authJwtController.isAuthenticated, function (req, res){
+            console.log(req.body);
+            res = res.status(200);
+            if(req.get('content-Type')) {
+                res = res.type(req.get('content-Type'));
+            }
+            var o = getJSONObjectForMovieRequirement(req);
+            res.json(o);
+        }
+    );
+router.route('/movie/:movieId')
+    .delete(authController.isAuthenticated, function(req, res) {
+            const movie = movie.find(movie => movie.id.toString() === req.params.id);
+            res.status(200).json(movie);
+        }
+    )
+    .put(authjwtController.isAuthenticated, function(req, res) {
+            const movie = movie.find(movie => movie.id.toString() === req.params.id);
+            res.status(200).json(movie);
+        }
+    )
+    .get(authjwtController.isAuthenticated, function(req, res) {
+            const movie = movie.find(movie => movie.id.toString() === req.params.id);
+            res.status(200).json(movie);
+        }
+    );
+
+
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000);
